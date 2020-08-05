565b262e8d3df77b9d6723d1610c172041e05a05
Geektree0101 <h2s1880@gmail.com>
2020-08-05 22:28:08 +0900

    ready to development builder

--- worker/builder.go
+++ worker/builder.go
@@ -1,1 +1,45 @@
 package worker
+
+import (
+	"fmt"
+
+	"github.com/GeekTree0101/iSpygo/model"
+)
+
+// BuilderError is error for builder
+type BuilderError struct {
+	desc string
+}
+
+// Error is builder error constructor
+func (be *BuilderError) Error() string {
+	return fmt.Sprintf("%s", be.desc)
+}
+
+// Builder is spy object generator
+type Builder struct {
+	nodes []model.Node
+}
+
+// NewBuilder is builder constructor
+func NewBuilder(nodes []model.Node) Builder {
+	return Builder{
+		nodes: nodes,
+	}
+}
+
+// GetPresenter return presenter spy object
+func GetPresenter() (string, error) {
+
+	return "", &BuilderError{
+		desc: "unknown error",
+	}
+}
+
+// GetDisplayer return displayer spy object
+func GetDisplayer() (string, error) {
+
+	return "", &BuilderError{
+		desc: "unknown error",
+	}
+}
