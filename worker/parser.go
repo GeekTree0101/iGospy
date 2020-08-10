24f442671a8639bf0f92beaf6b74b26651ddee46
Geektree0101 <h2s1880@gmail.com>
2020-08-10 23:24:42 +0900

    add parser

--- worker/parser.go
+++ worker/parser.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/GeekTree0101/iSpygo/model"
 )
@@ -26,9 +27,91 @@ func NewParser() Parser {
 }
 
 // Processing make a node-trees
-func (p *Parser) Processing(str string) ([]model.Node, error) {
-	// TODO:
-	return nil, &ParserError{
-		desc: "unknown error",
+func (p *Parser) Processing(str string) (model.Node, error) {
+
+	var syntex string = ""
+	var syntexQueue []string
+	var root model.Node
+	var children []model.Node
+	var currentDepth int = 0
+
+	// FIXME: processing needs depth inference logic
+
+	for _, r := range strings.TrimSpace(str) {
+
+		char := string(r)
+
+		switch char {
+		case "{":
+			trimmedSyntex := strings.TrimSpace(syntex)
+			syntexQueue = append(syntexQueue, trimmedSyntex)
+			syntex = ""
+			currentDepth++
+
+		case "}":
+			if len(syntexQueue) == 0 {
+				return root, &ParserError{
+					desc: "queue is empty",
+				}
+			}
+
+			lastSyntex := syntexQueue[len(syntexQueue)-1]
+			splitSyntex := strings.Split(lastSyntex, " ")
+
+			if len(splitSyntex) != 2 {
+				return root, &ParserError{
+					desc: "invalid syntex structure",
+				}
+			}
+
+			name := splitSyntex[len(splitSyntex)-1]
+
+			switch currentDepth {
+			case 1:
+				root = model.Node{
+					Type:     model.Title,
+					Name:     name,
+					Children: children,
+				}
+
+			case 2:
+				var bros []model.Node
+				var child []model.Node
+
+				for _, c := range children {
+					if c.Type == model.Behavior {
+						child = append(child, c)
+					} else {
+						bros = append(bros, c)
+					}
+				}
+
+				children = append(bros, model.Node{
+					Type:     model.Context,
+					Name:     name,
+					Children: child,
+				})
+
+			case 3:
+				children = append(children, model.Node{
+					Type:     model.Behavior,
+					Name:     name,
+					Children: nil,
+				})
+
+			default:
+				break
+			}
+
+			syntexQueue = syntexQueue[:len(syntexQueue)-1]
+			currentDepth--
+
+		default:
+			if currentDepth < 3 && char != "\n" {
+				syntex += char
+			}
+		}
 	}
+
+	return root, nil
 }
--- worker/parser_test.go
+++ worker/parser_test.go
@@ -2,28 +2,306 @@ package worker_test
 
 import (
 	"testing"
+
+	"github.com/GeekTree0101/iSpygo/model"
+
+	"github.com/GeekTree0101/iSpygo/worker"
 )
 
 func Test_Processing_Valid_And_Simple_Usecase(t *testing.T) {
 	// given
+	var rawstr string = `
+	enum Feed {
+
+		enum Reload {
+
+			enum Req {
+
+			}
+
+			enum Res {
+
+				var cards: [Card]
+				var error: Error?
+			}
+
+			enum ViewModel {
+
+				var error: Error?
+			}
+		}
+
+		enum Next {
+
+			enum Req {
+
+			}
+
+			struct Res {
+
+				var cards: [Card]
+				var error: Error?
+			}
+
+			struct ViewModel {
+
+				var changeSet: [Change<CardViewModel>]
+				var error: Error?
+			}
+		}
+	}
+	`
+
+	parser := worker.NewParser()
 
 	// when
+	result, err := parser.Processing(rawstr)
 
 	// then
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// title
+
+	if result.Type != model.Title {
+		t.Errorf("root is not Title")
+		return
+	}
+
+	if len(result.Children) != 2 {
+		t.Errorf("children count doesn't 2, %d", len(result.Children))
+		return
+	}
+
+	// context
+
+	if result.Children[0].Type != model.Context {
+		t.Errorf("%s type doesn't Context", result.Children[0].Name)
+		return
+	}
+
+	if result.Children[1].Type != model.Context {
+		t.Errorf("%s type doesn't Context", result.Children[1].Name)
+		return
+	}
+
+	if len(result.Children[0].Children) != 3 {
+		t.Errorf("children count doesn't 3, %d", len(result.Children[0].Children))
+		return
+	}
+
+	// first context
+
+	if result.Children[0].Children[0].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[0].Name)
+		return
+	}
+
+	if result.Children[0].Children[1].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[1].Name)
+		return
+	}
+
+	if result.Children[0].Children[2].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[2].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[0].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[0].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[1].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[1].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[2].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[2].Name)
+		return
+	}
+
+	// second context
+
+	if result.Children[1].Children[0].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[0].Name)
+		return
+	}
+
+	if result.Children[1].Children[1].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[1].Name)
+		return
+	}
+
+	if result.Children[1].Children[2].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[2].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[0].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[0].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[1].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[1].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[2].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[2].Name)
+		return
+	}
 }
 
 func Test_Processing_Valid_And_Complicated_Usecase(t *testing.T) {
 	// given
+	var rawstr string = `
+	enum Feed {
+
+		enum Reload {
+
+			enum Req {
+
+			}
+
+			enum Res {
+
+				var cards: [Card]
+				var error: Error?
+			}
+
+			enum ViewModel {
+
+				var error: Error?
+			}
+		}
+
+		enum Next {
+
+			enum Req {
+
+			}
+
+			struct Res {
+
+				var cards: [Card]
+				var error: Error?
+			}
+
+			struct ViewModel {
+
+				var changeSet: [Change<CardViewModel>]
+				var error: Error?
+			}
+		}
+	}
+	`
+
+	parser := worker.NewParser()
 
 	// when
+	result, err := parser.Processing(rawstr)
 
 	// then
-}
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-func Test_Processing_Invalid_Usecase(t *testing.T) {
-	// given
+	// title
 
-	// when
+	if result.Type != model.Title {
+		t.Errorf("root is not Title")
+		return
+	}
 
-	// then
+	if len(result.Children) != 2 {
+		t.Errorf("children count doesn't 2, %d", len(result.Children))
+		return
+	}
+
+	// context
+
+	if result.Children[0].Type != model.Context {
+		t.Errorf("%s type doesn't Context", result.Children[0].Name)
+		return
+	}
+
+	if result.Children[1].Type != model.Context {
+		t.Errorf("%s type doesn't Context", result.Children[1].Name)
+		return
+	}
+
+	if len(result.Children[0].Children) != 3 {
+		t.Errorf("children count doesn't 3, %d", len(result.Children[0].Children))
+		return
+	}
+
+	// first context
+
+	if result.Children[0].Children[0].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[0].Name)
+		return
+	}
+
+	if result.Children[0].Children[1].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[1].Name)
+		return
+	}
+
+	if result.Children[0].Children[2].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[0].Children[2].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[0].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[0].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[1].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[1].Name)
+		return
+	}
+
+	if len(result.Children[0].Children[2].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[0].Children[2].Name)
+		return
+	}
+
+	// second context
+
+	if result.Children[1].Children[0].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[0].Name)
+		return
+	}
+
+	if result.Children[1].Children[1].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[1].Name)
+		return
+	}
+
+	if result.Children[1].Children[2].Type != model.Behavior {
+		t.Errorf("%s type doesn't Behavior", result.Children[1].Children[2].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[0].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[0].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[1].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[1].Name)
+		return
+	}
+
+	if len(result.Children[1].Children[2].Children) != 0 {
+		t.Errorf("%s children should be zero", result.Children[1].Children[2].Name)
+		return
+	}
 }
